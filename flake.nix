0c30f678ec8b0e5e5ffa72fac5039d26c5f789dc
ethanabrooks <ethanabrooks@gmail.com>
2023-06-16 14:56:25 -0700

    remove python stuff add node stuff

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/";
+    utils.url = "github:numtide/flake-utils/";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }: let
+    out = system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          alejandra
+          nodejs_20
+          ngrok
+        ];
+        PYTHONBREAKPOINT = "ipdb.set_trace";
+        shellHook = ''
+          set -o allexport
+          source .env
+          set +o allexport
+        '';
+      };
+    };
+  in
+    utils.lib.eachDefaultSystem out;
+}
