8123abaf94dfcd0ac6dcb85a9f5be5251ed5a741
ethanabrooks <ethanabrooks@gmail.com>
2023-06-19 12:19:14 -0700

    remove unused dependencies

--- scripts/app.ts
+++ scripts/app.ts
@@ -3,13 +3,8 @@ import express from 'express';
 import {
   InteractionType,
   InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
 } from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
-import { updateLanguageServiceSourceFile } from 'typescript';
+import { VerifyDiscordRequest } from './utils.js';
 
 // Create an express app
 const app = express();
