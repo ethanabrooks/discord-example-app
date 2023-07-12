cfedee5ff73b733f1765515a3e4008a80ad059e5
ethanabrooks <ethanabrooks@gmail.com>
2023-07-12 16:33:34 -0700

    extract replyWithGPTCompletion

--- scripts/commands.ts
+++ scripts/commands.ts
@@ -7,6 +7,7 @@ import {
   AttachmentBuilder,
   EmbedBuilder,
   CommandInteraction,
+  Message,
 } from "discord.js";
 import createChatCompletionWithBackoff from "./gpt.js";
 import { ChatCompletionRequestMessage } from "openai";
@@ -26,6 +27,92 @@ type Options = {
   speak?: boolean;
 };
 
+// Buttons
+type ButtonComponents = {
+  id: string;
+  label: string;
+  style: ButtonStyle;
+};
+const buttons = {
+  submit: {
+    id: "submit",
+    label: "Submit to GPT",
+    style: ButtonStyle.Primary,
+  },
+  visualize: {
+    id: "visualize",
+    label: "Visualize",
+    style: ButtonStyle.Secondary,
+  },
+};
+
+async function replyWithGPTCompletion(
+  interaction: CommandInteraction,
+  firstReply: boolean,
+  speak: boolean,
+) {
+  const channel = interaction.channel;
+  let content: string;
+  if (channel == null) {
+    content = "Error: Channel not found";
+  } else {
+    const messages = await interaction.channel.messages
+      .fetch({
+        limit: 100,
+        cache: false,
+      })
+      .then((messages) =>
+        messages.reverse().map(
+          (message): ChatCompletionRequestMessage => ({
+            role:
+              // message.interaction != null &&
+              // Just check if author id matches bot id
+              message.author.id === clientId ? "system" : "user",
+            content: message.content,
+          }),
+        ),
+      )
+      .catch(console.error);
+    if (messages instanceof Object) {
+      console.log(messages);
+
+      if (firstReply) {
+        await interaction.deferReply();
+      }
+
+      // Query GPT
+      let excess: string;
+      if (speak === true) {
+        console.log(messages);
+        const completion = await createChatCompletionWithBackoff(messages);
+        if (completion === undefined) {
+          [content, excess] = ["Error: GPT-3 API call failed", ""];
+        } else {
+          [content, excess] = splitAtResponseLimit(completion);
+        }
+
+        // content = 'Test' + counter; // Debug
+      } else {
+        content = "The visualization is above.";
+      }
+    } else {
+      content = "Error: Failed to fetch messages";
+    }
+  }
+  console.log(content);
+
+  const row = Object.values(buttons)
+    .map(({ id, label, style }: ButtonComponents) =>
+      new ButtonBuilder().setCustomId(id).setLabel(label).setStyle(style),
+    )
+    .reduce(
+      (row, button) => row.addComponents(button),
+      new ActionRowBuilder<ButtonBuilder>(),
+    );
+
+  return { content, components: [row] };
+}
+
 // Create commands
 export const Commands = [
   {
@@ -36,96 +123,17 @@ export const Commands = [
       interaction: CommandInteraction,
       { firstReply = true, speak = true }: Options = {},
     ) {
-      const channel = interaction.channel;
-      let content: string;
-      if (channel == null) {
-        content = "Error: Channel not found";
-      } else {
-        const messages = await interaction.channel.messages
-          .fetch({
-            limit: 100,
-            cache: false,
-          })
-          .then((messages) =>
-            messages.reverse().map(
-              (message): ChatCompletionRequestMessage => ({
-                role:
-                  // message.interaction != null &&
-                  // Just check if author id matches bot id
-                  message.author.id === clientId ? "system" : "user",
-                content: message.content,
-              }),
-            ),
-          )
-          .catch(console.error);
-        if (messages instanceof Object) {
-          console.log(messages);
-
-          if (firstReply) {
-            await interaction.deferReply();
-          }
-
-          // Query GPT
-          let excess: string;
-          if (speak === true) {
-            console.log(messages);
-            const completion = await createChatCompletionWithBackoff(messages);
-            if (completion === undefined) {
-              [content, excess] = ["Error: GPT-3 API call failed", ""];
-            } else {
-              [content, excess] = splitAtResponseLimit(completion);
-            }
-
-            // content = 'Test' + counter; // Debug
-          } else {
-            content = "The visualization is above.";
-          }
-        } else {
-          content = "Error: Failed to fetch messages";
-        }
-      }
-      console.log(content);
-
-      // Buttons
-      type ButtonComponents = {
-        id: string;
-        label: string;
-        style: ButtonStyle;
-      };
-      const buttons = {
-        submit: {
-          id: "submit",
-          label: "Submit to GPT",
-          style: ButtonStyle.Primary,
-        },
-        visualize: {
-          id: "visualize",
-          label: "Visualize",
-          style: ButtonStyle.Secondary,
-        },
-      };
-
-      const row = Object.values(buttons)
-        .map(({ id, label, style }: ButtonComponents) =>
-          new ButtonBuilder().setCustomId(id).setLabel(label).setStyle(style),
-        )
-        .reduce(
-          (row, button) => row.addComponents(button),
-          new ActionRowBuilder<ButtonBuilder>(),
-        );
-
+      const reply = await replyWithGPTCompletion(
+        interaction,
+        firstReply,
+        speak,
+      );
       // Update reply
       let response: Message; // Better way to do it?
       if (firstReply) {
-        response = await interaction.followUp({
-          content,
-          components: [row],
-        });
+        response = await interaction.followUp(reply);
       } else {
-        response = await interaction.channel.send({
-          content,
-          components: [row],
-        });
+        response = await interaction.channel.send(reply);
       }
 
       // Button interaction
@@ -135,10 +143,7 @@ export const Commands = [
         // Send new message
         switch (confirmation.customId) {
           case buttons.submit.id:
-            await confirmation.update({
-              content,
-              components: [],
-            });
+            await confirmation.update({ ...reply, components: [] });
             await this.execute(interaction, { firstReply: false });
             break;
           case buttons.visualize.id:
@@ -155,10 +160,7 @@ export const Commands = [
             });
 
             // Clear
-            await confirmation.update({
-              content,
-              components: [],
-            });
+            await confirmation.update(reply);
             await this.execute(interaction, {
               firstReply: false,
               speak: false,
