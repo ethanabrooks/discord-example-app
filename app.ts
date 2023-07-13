d3654c16a9f94ad7ddd919f77524ea54245024c7
ethanabrooks <ethanabrooks@gmail.com>
2023-07-13 09:27:43 -0700

    remove unused imports in app.ts

--- app.ts
+++ app.ts
@@ -1,14 +1,7 @@
-import 'dotenv/config';
-import express from 'express';
-import {
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
+import "dotenv/config";
+import express from "express";
+import { InteractionType, InteractionResponseType } from "discord-interactions";
+import { VerifyDiscordRequest, getRandomEmoji } from "./utils.js";
 
 // Create an express app
 const app = express();
@@ -17,13 +10,10 @@ const PORT = process.env.PORT || 3000;
 // Parse request body and verifies incoming requests using discord-interactions package
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-// Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
-
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', async function (req, res) {
+app.post("/interactions", async function (req, res) {
   // Interaction type and data
   const { type, id, data } = req.body;
 
@@ -42,13 +32,13 @@ app.post('/interactions', async function (req, res) {
     const { name } = data;
 
     // "test" command
-    if (name === 'test') {
+    if (name === "test") {
       // Send a message into the channel where command was triggered from
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           // Fetches a random emoji to send from a helper function
-          content: 'Hello world ' + getRandomEmoji(),
+          content: "Hello world " + getRandomEmoji(),
         },
       });
     }
@@ -56,5 +46,5 @@ app.post('/interactions', async function (req, res) {
 });
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+  console.log("Listening on port", PORT);
 });
